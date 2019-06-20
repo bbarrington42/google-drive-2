367795319cb7097ee735106fab31bfaaf00742fa
Bill Barrington <bill.barrington@candidpartners.com>
2019-06-20 14:49:31 -0400

    Cleaned up 'create_metadata'. Now returns a Future Maybe id.

--- src/api.js
+++ src/api.js
@@ -63,10 +63,10 @@ const move_file = fromParentId => toParentId => fileId => client.buildRequest ({
 // This creates only the metadata for a new file
 // Required properties:
 // id, mimeType, name, parent folder id
-const create_metadata = folderId => mimeType => name =>
-    // If for some reason an ID can't be created, then this fails
-    Future.chain (maybeId => {
-        const rv = S.map (id => {
+// returns a Future Maybe id
+const create_metadata = folderId => mimeType => name => S.pipe ([
+    Future.map (res => S.head (res.data.ids)),
+    S.chain (S.traverse (Future) (id => {
             const options = {
                 method: 'POST',
                 kind: 'drive#file',
@@ -78,13 +78,14 @@ const create_metadata = folderId => mimeType => name =>
                 }
             };
             return client.buildRequest (options) ('/drive/v3/files');
-        }) (maybeId);
-        return S.fromMaybe (Future.resolve (Error (`Could not generate ID for ${name}: ${mimeType}`))) (rv);
-    }) (Future.map (res => S.head (res.data.ids)) (ids_for_file));
+        })
+    ),
+    Future.map(S.map(res => res.data.id))
+]) (ids_for_file);
 
 
 // Returns a Future of an array of (possibly empty) file IDs
-const find_file = mimeType => folderId => name => {
+const find_file = folderId => mimeType => name => {
     const query = `name = '${name}' and '${folderId}' in parents and mimeType = '${mimeType}'`;
     const options = {params: {q: query}};
     return S.map (res => S.map (file => file.id) (res.data.files)) (list_files (options));
@@ -99,8 +100,14 @@ const upload_contents = fileId => data => {
         },
         data
     };
-    return client.buildRequest(options) (`/upload/drive/v3/files/${fileId}`);
+    return client.buildRequest (options) (`/upload/drive/v3/files/${fileId}`);
 };
+
+// const find_or_create_file = folderId => mimeType => name => data => {
+//   // If more than one file is found, the first returned will be used
+//   const maybeId = S.map(S.head)(find_file(folderId)(mimeType)(name));
+//   return S.chain(S.maybe(create_metadata(folderId)(mimeType)(name))(id => upload_contents(id)(data)))(maybeId);
+// };
 
 
 const ids_for_file = client.buildRequest ({
@@ -122,8 +129,8 @@ module.exports = {
 // 1E3CTgo_oIAGM2rFiP-6oki98X9qPpY36  (test.json)
 const Future = require ('fluture');
 //Future.fork (console.error, res => console.log(res.data.files)) (list_files());
-//Future.fork (console.error, console.log) (create_metadata ('1iRprWI2mA8BvVU8cj3CRybkrmC0vvdQb') ('application/json')
+Future.fork (console.error, console.log) (create_metadata ('1iRprWI2mA8BvVU8cj3CRybkrmC0vvdQb') ('application/json') ('test.json'));
+//Future.fork (console.error, console.log) (find_file ('application/json') ('1iRprWI2mA8BvVU8cj3CRybkrmC0vvdQb')
 // ('test.json'));
-Future.fork (console.error, console.log) (find_file ('application/json') ('1iRprWI2mA8BvVU8cj3CRybkrmC0vvdQb') ('test.json'));
 
 //Future.fork (console.error, console.log) (upload_contents('1E3CTgo_oIAGM2rFiP-6oki98X9qPpY36') ('{}'));
