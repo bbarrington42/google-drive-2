16844e99141d4620e9365896f098b509d1539c6b
Bill Barrington <bill.barrington@candidpartners.com>
2019-06-25 14:11:42 -0400

    Defined equality for receipt entries and statement entries.

--- src/reconcile.js
+++ src/reconcile.js
@@ -16,17 +16,32 @@ const {runFuture} = require ('../lib/misc');
 
 const Future = require ('fluture');
 
+// Equality for a receipt and a statement entry
+// Both object types have the same fields (name, date, & amount)
+// However, the receipt values are string arrays whereas the statement values are single strings
+const isEqual = receipt => statement => {
+    const match = receipt => statement => S.any (S.equals (statement)) (receipt);
+    return match (receipt.amount) (statement.amount) ? (match (receipt.date) (statement.date) ?
+        S.any (str => statement.name.includes(str)) (receipt.name) : false) : false;
+};
+
 
 // Download contents of master.json and generate a summary report
 const getJson = S.chain (folder =>
-    readJson (S.maybe ('') (S.prop('id')) (folder)) ('application/json') ('master.json'));
+    readJson (S.maybe ('') (S.prop ('id')) (folder)) ('application/json') ('master.json'));
 
 const json = getJson (getFolder ('Receipts'));
 
 const receipts = S.map (S.maybe ([]) (summary)) (json);
 
+//runFuture () (receipts);
+
 // todo Get the charges from the statement and reconcile with the receiptsSummary
 // todo For now just use this file...
-const charges = getCharges('../data/amex-statement-jun-17.csv');
+const charges = getCharges ('../data/amex-statement-jun-17.csv');
+//runFuture()(charges);
 
-// todo Reconcile receipts against charges
+// Reconciliation
+// For each receipt, search for a match in the statements array
+
+
