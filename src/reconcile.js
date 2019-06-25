3f108d56bb108e6f10e103f837994bd615303223
Bill Barrington <bill.barrington@candidpartners.com>
2019-06-25 19:31:05 -0400

    Working out details of reconciliation.

--- src/reconcile.js
+++ src/reconcile.js
@@ -19,10 +19,16 @@ const Future = require ('fluture');
 // Equality for a receipt and a statement entry
 // Both object types have the same fields (name, date, & amount)
 // However, the receipt values are string arrays whereas the statement values are single strings
-const isEqual = receipt => statement => {
+const isEqual = statement => receipt => {
     const match = receipt => statement => S.any (S.equals (statement)) (receipt);
     return match (receipt.amount) (statement.amount) ? (match (receipt.date) (statement.date) ?
-        S.any (str => statement.name.includes(str)) (receipt.name) : false) : false;
+        S.any (str => statement.name.includes (str)) (receipt.name) : false) : false;
+};
+
+
+const unAccounted = receipts => statement => {
+    const reconciled = entry => S.isJust (S.find (isEqual (entry)) (receipts));
+    return S.reject (reconciled) (statement);
 };
 
 
@@ -44,4 +50,21 @@ const charges = getCharges ('../data/amex-statement-jun-17.csv');
 // Reconciliation
 // For each receipt, search for a match in the statements array
 
+// testing
+const a = [
+    {
+        name: 'blar',
+        date: '06/25/2019',
+        amount: '25.25'
+    }
+];
 
+const b = [
+    {
+        name: ['blart'],
+        date: ['06/25/2019'],
+        amount: ['25.25']
+    }
+];
+
+console.log (unAccounted (b) (a));
